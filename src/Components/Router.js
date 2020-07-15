114f7c1c0316001a4cf559d121f8031c327384fb
gangslee <37660333+gangslee@users.noreply.github.com>
2020-07-16 05:54:34 +0900

    Set SearchContainer.js

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,14 +1,10 @@
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Redirect,
-  Switch,
-} from "react-router-dom";
-import Home from "Routes/Home";
-import TV from "Routes/TV";
-import Search from "Routes/Search";
-import Header from "Components/Header";
+import React from 'react';
+import {BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
+import Home from 'Routes/Home';
+import TV from 'Routes/TV';
+import Search from 'Routes/Search';
+import Detail from 'Routes/Detail';
+import Header from 'Components/Header';
 
 export default () => (
   <Router>
@@ -18,6 +14,8 @@ export default () => (
         <Route path="/" exact component={Home} />
         <Route path="/tv" exact component={TV} />
         <Route path="/search" component={Search} />
+        <Route path="/movie/:id" component={Detail} />
+        <Route path="/show/:id" component={Detail} />
         <Redirect from="*" to="/" />
       </Switch>
     </>
