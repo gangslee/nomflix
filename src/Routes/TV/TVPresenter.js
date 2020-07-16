d434e49741a2c4715075fc9fd0ecbc00f2f2a891
gangslee <37660333+gangslee@users.noreply.github.com>
2020-07-17 06:22:46 +0900

    Set Message.js

--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import Section from 'Components/Section';
 import Loader from 'Components/Loader';
+import Message from 'Components/Message';
 
 const Container = styled.div`
   padding: 0 20px;
@@ -22,6 +23,7 @@ const TVPresenter = ({topRated, popular, airingToday, loading, error}) =>
       {airingToday && airingToday.length > 0 && (
         <Section title="Airing Today">{airingToday.map((show) => show.name)}</Section>
       )}
+      {error && <Message color="#e74c3c" text={error} />}
     </Container>
   );
 
